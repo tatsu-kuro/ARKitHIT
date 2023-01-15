4316fbb0607fe24ca816baa8a5dfa8bdbb3d07c3
tatsuaki <tatsuakikuroda@me.com>
2023-01-15 14:41:14 +0900

    setteiPage修正

--- ARKitHIT/SetteiViewController.swift
+++ ARKitHIT/SetteiViewController.swift
@@ -48,138 +48,68 @@ class SetteiViewController: UIViewController {
         UserDefaults.standard.set(100, forKey: "multiFace")
     }
     
-//    @IBAction func widthRangeButton(_ sender: Any) {
-//        widthRange = Field2value(field:B2CInput)
-//
-    
-    
     @IBAction func onTapGestureEye(_ sender: Any) {
-
-        print("eyetap")
+        multiEye.backgroundColor = UIColor.systemGray3
         let alert = UIAlertController(title: "", message: "", preferredStyle: .alert)
         let saveAction = UIAlertAction(title: "OK", style: .default) { [self] (action:UIAlertAction!) -> Void in
             // 入力したテキストをコンソールに表示
+            multiEye.backgroundColor = UIColor.white
             let textField = alert.textFields![0] as UITextField
-//            let ettString:String = textField.text!
-//            let value=Int(textField.text!)
             if Int(textField.text!) != nil{//camera.checkEttString(ettStr: ettString){
                 multiEye.text=textField.text
                 UserDefaults.standard.set(Int(textField.text!), forKey: "multiEye")
-
+                
             }else{
                 let dialog = UIAlertController(title: "", message: "0123456789 are available.", preferredStyle: .alert)
                 //ボタンのタイトル
                 dialog.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
                 //実際に表示させる
                 self.present(dialog, animated: true, completion: nil)
-//                print(",:0123456789以外は受け付けません")
             }
-//            print("\(String(describing: textField.text))")
         }
-        
         let cancelAction = UIAlertAction(title: "Cancel", style: .default) { (action:UIAlertAction!) -> Void in
+            self.multiEye.backgroundColor = UIColor.white
         }
         // UIAlertControllerにtextFieldを追加
         alert.addTextField { (textField:UITextField!) -> Void in
             textField.keyboardType = UIKeyboardType.numberPad//numbersAndPunctuation// default//.numberPad
-
         }
         alert.addAction(cancelAction)//この行と下の行の並びを変えるとCancelとOKの左右が入れ替わる。
         alert.addAction(saveAction)
         present(alert, animated: true, completion: nil)
-        
     }
-    /*
-     @IBAction func tapOnEttText(_ sender: Any) {
-         print("eyetap")
-         let alert = UIAlertController(title: "", message: "", preferredStyle: .alert)
-         let saveAction = UIAlertAction(title: "OK", style: .default) { [self] (action:UIAlertAction!) -> Void in
-             // 入力したテキストをコンソールに表示
-             let textField = alert.textFields![0] as UITextField
-             let ettString:String = textField.text!
-
-             if camera.checkEttString(ettStr: ettString){
-                 if ettMode==0{
-                     ettModeText0=ettString
-                 }else if ettMode==1{
-                     ettModeText1=ettString
-                 }else if ettMode==2{
-                     ettModeText2=ettString
-                 }else{
-                     ettModeText3=ettString
-                 }
-                 setUserDefaults()
-                 ettText.text=ettString
-             }else{
-                 let dialog = UIAlertController(title: "", message: "0123456789,: are available.\nlike as 1,1:2:15,2:2:15", preferredStyle: .alert)
-                 //ボタンのタイトル
-                 dialog.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                 //実際に表示させる
-                 self.present(dialog, animated: true, completion: nil)
- //                print(",:0123456789以外は受け付けません")
-             }
- //            print("\(String(describing: textField.text))")
-         }
-         
-         let cancelAction = UIAlertAction(title: "Cancel", style: .default) { (action:UIAlertAction!) -> Void in
-         }
-         // UIAlertControllerにtextFieldを追加
-         alert.addTextField { (textField:UITextField!) -> Void in
-             textField.keyboardType = UIKeyboardType.numbersAndPunctuation// default//.numberPad
-             if self.ettMode==0{
-                 textField.text=self.ettModeText0
-             }else if self.ettMode==1{
-                 textField.text=self.ettModeText1
-             }else if self.ettMode==2{
-                 textField.text=self.ettModeText2
-             }else{
-                 textField.text=self.ettModeText3
-             }
-         }
-         alert.addAction(cancelAction)//この行と下の行の並びを変えるとCancelとOKの左右が入れ替わる。
-         alert.addAction(saveAction)
-         present(alert, animated: true, completion: nil)
-         
-     }
-     */
-    
-    
-    
     
     @IBAction func onTapGestureFace(_ sender: Any) {
-        print("face")
+        multiFace.backgroundColor = UIColor.systemGray3
         let alert = UIAlertController(title: "", message: "", preferredStyle: .alert)
         let saveAction = UIAlertAction(title: "OK", style: .default) { [self] (action:UIAlertAction!) -> Void in
             // 入力したテキストをコンソールに表示
+            multiFace.backgroundColor = UIColor.white
             let textField = alert.textFields![0] as UITextField
-//            let ettString:String = textField.text!
-//            let value=Int(textField.text!)
             if Int(textField.text!) != nil{//camera.checkEttString(ettStr: ettString){
                 multiFace.text=textField.text
                 UserDefaults.standard.set(Int(textField.text!), forKey: "multiFace")
+                
             }else{
                 let dialog = UIAlertController(title: "", message: "0123456789 are available.", preferredStyle: .alert)
                 //ボタンのタイトル
                 dialog.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
                 //実際に表示させる
                 self.present(dialog, animated: true, completion: nil)
-//                print(",:0123456789以外は受け付けません")
             }
-//            print("\(String(describing: textField.text))")
         }
-        
         let cancelAction = UIAlertAction(title: "Cancel", style: .default) { (action:UIAlertAction!) -> Void in
+            self.multiFace.backgroundColor = UIColor.white
         }
         // UIAlertControllerにtextFieldを追加
         alert.addTextField { (textField:UITextField!) -> Void in
             textField.keyboardType = UIKeyboardType.numberPad//numbersAndPunctuation// default//.numberPad
-
         }
         alert.addAction(cancelAction)//この行と下の行の並びを変えるとCancelとOKの左右が入れ替わる。
         alert.addAction(saveAction)
         present(alert, animated: true, completion: nil)
-        
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let ww=view.bounds.width
@@ -194,10 +124,10 @@ class SetteiViewController: UIViewController {
         multiEye.text=getUserDefault(str:"multiEye" , ret:100).description
         multiFace.text=getUserDefault(str:"multiFace" , ret:100).description
         
-        setLabelProperty(multiEye, x: sp*2.5+bw*0.5, y: by0, w: bw, h: wh/30, UIColor.systemGray5)
+        setLabelProperty(multiEye, x: sp*2.5+bw*0.5, y: by0, w: bw, h: wh/30, UIColor.white)
         multiEyeText.frame=CGRect(x: multiEye.frame.maxX+sp, y: by0, width: bw*10, height: wh/30)
 
-        setLabelProperty(multiFace,x:sp*2.5+bw*0.5,y:by0+sp+wh/30,w: bw,h: wh/30,UIColor.systemGray5)
+        setLabelProperty(multiFace,x:sp*2.5+bw*0.5,y:by0+sp+wh/30,w: bw,h: wh/30,UIColor.white)
         multiFaceText.frame=CGRect(x: multiEye.frame.maxX+sp, y: by0+sp+wh/30, width: bw*10, height: wh/30)
 
 //            setButtonProperty(mailButton,x:sp*2.5+bw*0.5,y:by1,w:bw,h:bh,UIColor.blue)
